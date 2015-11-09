e40c73642f3dce552b73c36bbb8fca758a2ee836
cybergames1 <jtliu.tju@gmail.com>
2015-11-10 00:28:28 +0800

    Initial Commit

---
+++ CaptureVideoController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptureVideoController
+//
+//  Created by jianting on 15/11/10.
+//  Copyright © 2015年 jianting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
